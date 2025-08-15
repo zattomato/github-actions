9c39cecf16b8b698d79a213cf3c94f6deba4f27f
Izzat Hafizuddin Azmi <izzathafizuddin.azmi@dell.com>
2025-08-15 17:22:13 +0800

    lint.yml failing on any issues

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   return (
     <>
+
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
